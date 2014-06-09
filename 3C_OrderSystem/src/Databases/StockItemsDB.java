ec0fc7924b3728147bceb24961dc6e772d40aa75
syemp_000 <syemp_000@scottsPC>
2014-06-09 16:38:59 -0400

    updated StockItemsDB functions [Bellas]; deleted getAllStock();

--- 3C_OrderSystem/src/Databases/StockItemsDB.java
+++ 3C_OrderSystem/src/Databases/StockItemsDB.java
@@ -99,34 +99,7 @@ public class StockItemsDB {
         } catch (java.sql.SQLException e) {
             throw new TableException("Unable to create a new Order in the Database." + "\nDetail: " + e);
             }
-        }
-    
-    /**
-     * query for all Stock Items in the Product Inventory
-     * @return an Array List of Products in stock
-     * @throws Databases.StockItemsDB.TableException
-     * @throws Databases.StockItemsDB.TableException 
-     * @author Bella Belova
-     */
-    public java.util.ArrayList getAllStocks()
-            throws StockItemsDB.TableException, TableException{
-        java.sql.Statement stmt;
-        java.util.ArrayList results = null;
-        java.sql.ResultSet rs = null;
-        
-       /* try{
-          String createString = "select * from " + STOCK_ITEMS_TABLE_NAME + ";" ;                
-          stmt = mysqlConn.createStatement();
-          rs = stmt.executeQuery(createString);  
-          results = new java.util.ArrayList();
-            while (rs.next() == true)
-                results.add(new OrderSystem_Classes.Stock_Items (rs.getInt("PROD_ID"), rs.getString("PROD_NAME"), 
-                        rs.getInt("STOCK_QTY")));  
-        }catch (java.sql.SQLException e){
-            throw new TableException("Unable to search Quantity in Stock_Items Table." + "\nDetail: " + e);
-        } */
-        return results;
-    }       
+        }      
         
         /**
          * Query to search Stock Items Database for current Stock Quantity by PROD_ID.
